2789e7ee8fcd5a3fa2d5acf26194fda99f9041d3
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-29 16:30:07 -0300

    make a simple version of the tab buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
+import { useState } from 'react'
 import MovieGenre from './components/MovieGenre'
 import DATA from './data'
 
 function App() {
+  const [tabContent, setTabContent] = useState('')
+
+  function handleClick(title) {
+    console.log(title)
+    setTabContent(title)
+  }
+
   return (
     <>
       <h1>My favorite movies</h1>
       <h2>The best list ever is mine of course</h2>
-      <section class='genres-section'>
+      <section className='genres-section'>
         <h3>Movie Genre</h3>
         <div className='movie-genres'>
           {DATA.map((datum) => (
             <MovieGenre key={datum.title} {...datum} />
           ))}
         </div>
+      </section>
+      <section className='movie-reveal-section'>
+        {DATA.map((datum) => (
+          <button key={datum.title} onClick={() => handleClick(datum.title)}>
+            {datum.title}
+          </button>
+        ))}
+        {tabContent}
       </section>
     </>
   )
